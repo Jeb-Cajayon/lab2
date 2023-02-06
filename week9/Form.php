c7abffda0a64620e052b436047c5565b0595ef35
Jeb_Cajayon <jgcajayon@student.apc.edu.ph>
2023-02-06 22:50:16 +0800

    fixed forms so it can store data in the mysql database

--- week9/Form.php
+++ week9/Form.php
@@ -26,16 +26,49 @@ background-size: 100% auto;">
     </div>
 
     <?php
-    $name = $email = $gender = $comment = $website = "";
-    
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $name = test_input($_POST["name"]);
+      $nameErr = $emailErr = $genderErr = $websiteErr = "";
+      $name = $email = $gender = $comment = $website = "";
+      
+      if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        if (empty($_POST["name"])){
+          $nameErr = "Name is required";
+        }else{
+          $name = test_input($_POST["name"]);
+          if (!preg_match("/^[a-zA-Z-' ]*$/",$name)){
+            $nameErr = "Only letters and white space allowed";
+          }
+        }
+
+      if (empty($_POST["email"])){
+        $emailErr = "Email is required";
+      }else{
         $email = test_input($_POST["email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+          $emailErr = "Invalid email format";
+        }
+      }
+      
+      if (empty($_POST["website"])){
+        $website = "";
+      }else{
         $website = test_input($_POST["website"]);
+        if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+          $websiteErr = "Invalid URL";
+        }
+      }
+      
+      if (empty($_POST["comment"])) {
+        $comment = "";
+      }else{
         $comment = test_input($_POST["comment"]);
+      }
+      
+      if (empty($_POST["gender"])) {
+        $genderErr = "Gender is required";
+      }else{
         $gender = test_input($_POST["gender"]);
+      }
     }
-    
     function test_input($data) {
         $data = trim($data);
         $data = stripslashes($data);
@@ -43,24 +76,28 @@ background-size: 100% auto;">
         return $data;
     }
     ?>
-    
-    <h2>PHP Form Validation Example</h2>
-    
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    Name: <input type="text" name="name">
-    <br><br>
-    E-mail: <input type="text" name="email">
-    <br><br>
-    Website: <input type="text" name="website">
-    <br><br>
-    Comment: <textarea name="comment" rows="5" cols="40"></textarea>
-    <br><br>
-    Gender:
-    <input type="radio" name="gender" value="female">Female
-    <input type="radio" name="gender" value="male">Male
-    <input type="radio" name="gender" value="other">Other
-    <br><br>
-    <input type="submit" name="submit" value="Submit">
+
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website" value="<?php echo $website;?>">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
 </form>
 
 <?php
@@ -74,6 +111,39 @@ echo "<br>";
 echo $comment;
 echo "<br>";
 echo $gender;
+?>
+
+<?php
+//From mysql_insert.php
+// if statement below is for the MySQL insert code to execute only AFTER the submit button is pressed
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "newdb";
+	
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+	}
+	
+	$sql = "INSERT INTO myguests (name, website, email, comment, gender)
+	VALUES ('$name', '$website', '$email', '$comment', '$gender')";
+	
+	if ($conn->query($sql) === TRUE) {
+	echo "New record created successfully";
+	} else {
+	echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	
+	$conn->close();
+}	
+
 ?>
     
 </body>
